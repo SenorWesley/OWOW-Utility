b2c669fd23e15ff3d2fbdbb8e0c8781c5fa363b3
Wesley Peeters <wesley.peeters@student.gildeopleidingen.nl>
2017-06-06 15:24:29 +0200

    initial commit

---
+++ ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Utility
+//
+//  Created by Wesley Peeters on 06/06/2017.
+//  Copyright © 2017 owow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
